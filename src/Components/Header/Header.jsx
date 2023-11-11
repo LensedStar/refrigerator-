be38576561960a3da749ed43f65813063a21a816
LensedStar <daniil.letkovskyisp@gmail.com>
2023-11-11 20:45:51 +0100

    Add adaptation

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -1,17 +1,45 @@
-import React from "react";
+import React,{useState} from "react";
 
 import "./HeaderStyle.scss";
 
 import mailLogo from "../../Images/emailLogo.svg";
 import phoneLogo from "../../Images/phoneLogo.svg";
+import menu from "../../Images/menu.png";
+import close from  "../../Images/closeIcon.svg"
+
+import {Drawer} from "@mui/material";
+
 export default function Header() {
+    const [open, setOpen] = useState(false);
+    const handleClose = () => setOpen(false);
     return (
     <header className="header">
-        <nav className="navigation">
+        { window.innerWidth > 1000 ?
+       <nav className="navigation">
             <a href="#brands" className="nav__link">Brands</a>
             <a href="#about" className="nav__link">About</a>
             <a href="#contacts" className="nav__link">Contact</a>
         </nav>
+            :
+            <>
+                <button className="nav__menu" onClick={()=>setOpen(true)}>
+                    <img src={menu} alt="open side menu"/>
+                </button>
+                <Drawer
+                    open={open}
+                    onClose={() => setOpen(false)}
+                    anchor="left"
+                >
+
+                    <nav className="navigation__menu">
+                        <img className="menu__close" src={close} alt="close menu bar icon" role="button" onClick={()=>handleClose()} />
+                        <a onClick={()=>handleClose()} href="#brands" className="nav__link">Brands</a>
+                        <a onClick={()=>handleClose()} href="#about" className="nav__link">About</a>
+                        <a onClick={()=>handleClose()} href="#contacts" className="nav__link">Contact</a>
+                    </nav>
+                </Drawer>
+            </>
+        }
         <aside className="contacts__header">
             <article className="head__number">
                 <img src={phoneLogo} alt="phone"/>
